a681441d831ef658ebf0e990d1f40f0f8d789b1f
835230259@qq.com <jdk@jdk.com>
2016-05-31 20:14:22 +0800

    静态常量类改接口

--- app/src/main/java/com/jidekun/jdk/jdkplay/global/Api.java
+++ app/src/main/java/com/jidekun/jdk/jdkplay/global/Api.java
@@ -3,11 +3,19 @@ package com.jidekun.jdk.jdkplay.global;
 /**
  * Created by JDK on 2016/5/29.
  */
-public class Api {
+public interface Api {
     //服务器的主机地址
-    public static String SERVER_HOST = "http://127.0.0.1:8090/";
+    String SERVER_HOST = "http://127.0.0.1:8090/";
     //图片url的前缀 : http://127.0.0.1:8090/image?name=
-    public static String IMAGE_PREFIX = SERVER_HOST +"image?name=";
+    String IMAGE_PREFIX = SERVER_HOST + "image?name=";
     //home页的url地址
-    public static String HOME = SERVER_HOST + "home?index=";
+    String HOME = SERVER_HOST + "home?index=";
+    //app页的url地址
+    String App = SERVER_HOST + "app?index=";
+    //game页的url地址
+    String Game = SERVER_HOST + "game?index=";
+    //subject页的url地址
+    String Subject = SERVER_HOST + "subject?index=";
+    //recommend页的url地址
+    String Recommend = SERVER_HOST + "recommend?index=0";
 }
